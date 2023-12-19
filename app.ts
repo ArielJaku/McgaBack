f7fd951a5c04f139c92cb059287b4e0de8fb65b4
ArielJaku <ari_jaku@hotmail.com>
2023-12-18 22:38:08 -0300

    Conectando rutas, controladores y agregando middleware

--- app.ts
+++ app.ts
@@ -1,13 +1,31 @@
 import express from 'express';
+const mongoose = require('mongoose');
+const verdurasRoutes = require('./rutas/rutasVerdura')
 
 const app = express();
 
-const port = 3000;
+//conectar to mongo db
+const dbURI = 'mongodb+srv://ArielMon:Usuario123@cluster0.k3fgvjm.mongodb.net/note-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI,{ useNewUrlParser:true, useUnifiedTopology: true})
+.then(()=>app.listen(3000))
+.catch((error : any)=>console.log(error))
 
-app.use((req,res)=>{
-    res.send("ESTOY ESCUCHANDO EN EL PUERTO 3000")
+//middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}))
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    const validar : string = 'true'
+    res.setHeader('Access-Control-Allow-Credentials', validar);
+    next();
 });
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-  });
+app.use(express.json())
+app.use('/', verdurasRoutes);
+
+app.use((req,res)=>{
+    res.status(404)
+});
